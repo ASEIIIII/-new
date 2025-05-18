b1161e4f567d2bac86ed098c69a3398ba9ceddce
ASEIIIII <018s20289@keio.jp>
2025-05-18 02:27:54 +0000

    2

--- asei.c
+++ asei.c
@@ -3,22 +3,20 @@
 #include <string.h>
 #include <math.h>
 
-#define BUFSIZE 2000
+#define MAXLEN 2000
 #define MAXSEQ 1000
-#define LOG_BASE 2.0
 
-char *labels[4] = {"A", "C", "G", "T"};
+char *base_labels[4] = {"A", "C", "G", "T"};
 
 typedef struct {
-    char name[BUFSIZE];
-    char seq[BUFSIZE];
-} SEQ;
+    char name[MAXLEN];
+    char seq[MAXLEN];
+} Sequence;
 
-SEQ g_motif[MAXSEQ];
-SEQ g_pro[MAXSEQ];
+Sequence motifs[MAXSEQ];
+Sequence promoters[MAXSEQ];
 
-// A:0, C:1, G:2, T:3
-int base2num(char base){
+int base_to_index(char base){
     switch(base){
         case 'A': return 0;
         case 'C': return 1;
@@ -28,23 +26,22 @@ int base2num(char base){
     }
 }
 
-// モチーフ配列読み込み
-int read_multi_seq(char* filename){
+int load_motifs(char* filename){
     FILE *fp = fopen(filename, "r");
     if(fp == NULL){
-        printf("motif_file open error.\n");
+        perror("モチーフファイル開けなかった");
         exit(1);
     }
 
-    char line[BUFSIZE];
+    char line[MAXLEN];
     int count = 0;
-    while(fgets(line, BUFSIZE, fp) != NULL){
+    while(fgets(line, MAXLEN, fp) != NULL){
         if(line[0] == '>'){
-            strcpy(g_motif[count].name, line + 1);
-            g_motif[count].name[strcspn(g_motif[count].name, "\n")] = '\0';
+            strcpy(motifs[count].name, line + 1);
+            motifs[count].name[strcspn(motifs[count].name, "\n")] = '\0';
         } else {
             line[strcspn(line, "\n")] = '\0';
-            strcpy(g_motif[count].seq, line);
+            strcpy(motifs[count].seq, line);
             count++;
         }
     }
@@ -52,23 +49,22 @@ int read_multi_seq(char* filename){
     return count;
 }
 
-// プロモータ配列読み込み
-int read_promoter(char* filename){
+int load_promoters(char* filename){
     FILE *fp = fopen(filename, "r");
     if(fp == NULL){
-        printf("promoter_file open error.\n");
+        perror("プロモータファイル開けなかった");
         exit(1);
     }
 
-    char line[BUFSIZE];
+    char line[MAXLEN];
     int count = 0;
-    while(fgets(line, BUFSIZE, fp) != NULL){
+    while(fgets(line, MAXLEN, fp) != NULL){
         if(line[0] == '>'){
-            strcpy(g_pro[count].name, line + 1);
-            g_pro[count].name[strcspn(g_pro[count].name, "\n")] = '\0';
+            strcpy(promoters[count].name, line + 1);
+            promoters[count].name[strcspn(promoters[count].name, "\n")] = '\0';
         } else {
             line[strcspn(line, "\n")] = '\0';
-            strcpy(g_pro[count].seq, line);
+            strcpy(promoters[count].seq, line);
             count++;
         }
     }
@@ -76,129 +72,106 @@ int read_promoter(char* filename){
     return count;
 }
 
-// 頻度行列を作成
-void make_freq_table(int freqtable[4][BUFSIZE], int len, int seq_num){
+void calc_freq_table(int freq[4][MAXLEN], int motif_len, int motif_num){
     for(int i = 0; i < 4; i++)
-        for(int j = 0; j < len; j++)
-            freqtable[i][j] = 0;
+        for(int j = 0; j < motif_len; j++)
+            freq[i][j] = 0;
 
-    for(int i = 0; i < seq_num; i++){
-        for(int j = 0; j < len; j++){
-            int idx = base2num(g_motif[i].seq[j]);
-            if(idx >= 0) freqtable[idx][j]++;
+    for(int i = 0; i < motif_num; i++){
+        for(int j = 0; j < motif_len; j++){
+            int idx = base_to_index(motifs[i].seq[j]);
+            if(idx >= 0) freq[idx][j]++;
         }
     }
 }
 
-// log-oddsスコアを計算
-void make_odds_score(float q[4][BUFSIZE], int freqtable[4][BUFSIZE], int len, int seq_num){
+void calc_score_matrix(float score[4][MAXLEN], int freq[4][MAXLEN], int motif_len, int motif_num){
     float bg[4];
-    float total_bg = 7519429 + 4637676 + 4637676 + 7519429;
-    bg[0] = 7519429.0 / total_bg; // A
-    bg[1] = 4637676.0 / total_bg; // C
-    bg[2] = 4637676.0 / total_bg; // G
-    bg[3] = 7519429.0 / total_bg; // T
+    float total = 7519429 + 4637676 + 4637676 + 7519429;
+    bg[0] = 7519429.0 / total; 
+    bg[1] = 4637676.0 / total; 
+    bg[2] = 4637676.0 / total; 
+    bg[3] = 7519429.0 / total; 
 
-    for(int j = 0; j < len; j++){
+    for(int j = 0; j < motif_len; j++){
         for(int i = 0; i < 4; i++){
-            float pij = (freqtable[i][j] + 1.0) / (seq_num + 4.0); // 疑似頻度1の加算平滑化
-            q[i][j] = log(pij / bg[i]);
+            float prob = (freq[i][j] + 1.0) / (motif_num + 4.0); 
+            score[i][j] = log(prob / bg[i]);
         }
     }
 }
 
-// スコア出力（スコア5以上のすべて，なければ最大スコア1件）
-// 各プロモータごとに：スコア5.0以上をすべて表示，なければ最大スコア1件のみ表示
-void score_promoters(int gene_num, int motif_len, float q[4][BUFSIZE]){
-    printf("\n--- プロモータスコア（各プロモータごとに：スコア5.0以上すべて，なければ最大1件）---\n");
+void search_binding_sites(int promoter_count, int motif_len, float score_matrix[4][MAXLEN]){
+    for(int i = 0; i < promoter_count; i++){
+        char* pseq = promoters[i].seq;
+        int plen = strlen(pseq);
+        float best_score = -INFINITY;
+        int best_pos = -1;
 
-    for(int i = 0; i < gene_num; i++){
-        int seq_len = strlen(g_pro[i].seq);
-        float max_score = -INFINITY;
-        int max_pos = -1;
+        printf("\n%s\n", promoters[i].name);
 
-        // スコア5以上のものを一時保存
-        typedef struct {
-            int pos;
-            float score;
-        } ScoreEntry;
-
-        ScoreEntry entries[BUFSIZE];
-        int entry_count = 0;
-
-        for(int j = 0; j <= seq_len - motif_len; j++){
-            float score = 0.0;
+        for(int j = 0; j <= plen - motif_len; j++){
+            float score = 0;
             for(int k = 0; k < motif_len; k++){
-                int idx = base2num(g_pro[i].seq[j + k]);
-                if(idx < 0){
+                int b = base_to_index(pseq[j + k]);
+                if(b < 0){
                     score = -INFINITY;
                     break;
                 }
-                score += q[idx][k];
+                score += score_matrix[b][k];
             }
 
             if(score >= 5.0){
-                entries[entry_count].pos = j;
-                entries[entry_count].score = score;
-                entry_count++;
+                printf("  * ヒット位置: %d, スコア: %.2f\n", j, score);
             }
 
-            if(score > max_score){
-                max_score = score;
-                max_pos = j;
+            if(score > best_score){
+                best_score = score;
+                best_pos = j;
             }
         }
 
-        if(entry_count > 0){
-            for(int j = 0; j < entry_count; j++){
-                printf("%s\tpos: %d\tscore: %.2f\n", g_pro[i].name, entries[j].pos, entries[j].score);
-            }
-        } else {
-            printf("%s\tpos: %d\tscore: %.2f\n", g_pro[i].name, max_pos, max_score);
+        if(best_score < 5.0){
+            printf("  最も高いスコア: %.2f (位置: %d)\n", best_score, best_pos);
         }
     }
 }
 
-
-
-
 int main(int argc, char* argv[]){
     if(argc < 3){
-        printf("Usage: %s motif_file promoter_file\n", argv[0]);
+        fprintf(stderr, "使い方: %s モチーフファイル プロモータファイル\n", argv[0]);
         return 1;
     }
 
-    int seq_num = read_multi_seq(argv[1]);
-    int len = strlen(g_motif[0].seq);
+    int motif_num = load_motifs(argv[1]);
+    int motif_len = strlen(motifs[0].seq);
 
-    int freqtable[4][BUFSIZE];
-    float q[4][BUFSIZE];
+    int freq[4][MAXLEN];
+    float score[4][MAXLEN];
 
-    make_freq_table(freqtable, len, seq_num);
-    make_odds_score(q, freqtable, len, seq_num);
+    calc_freq_table(freq, motif_len, motif_num);
+    calc_score_matrix(score, freq, motif_len, motif_num);
 
-    // 頻度出力
-    printf("列ごとの塩基頻度：\n");
+    printf("塩基頻度表:\n");
     for(int i = 0; i < 4; i++){
-        printf("%s: ", labels[i]);
-        for(int j = 0; j < len; j++){
-            printf("%2d ", freqtable[i][j]);
+        printf("%s: ", base_labels[i]);
+        for(int j = 0; j < motif_len; j++){
+            printf("%2d ", freq[i][j]);
         }
         printf("\n");
     }
 
-    // ログオッズスコア出力
-    printf("\n列ごとのオッズスコア（log-odds）：\n");
+    printf("\nスコア行列（log-odds）:\n");
     for(int i = 0; i < 4; i++){
-        printf("%s: ", labels[i]);
-        for(int j = 0; j < len; j++){
-            printf("%5.2f ", q[i][j]);
+        printf("%s: ", base_labels[i]);
+        for(int j = 0; j < motif_len; j++){
+            printf("%5.2f ", score[i][j]);
         }
         printf("\n");
     }
 
-    int gene_num = read_promoter(argv[2]);
-    score_promoters(gene_num, len, q);
+    int promoter_count = load_promoters(argv[2]);
+    search_binding_sites(promoter_count, motif_len, score);
 
     return 0;
 }
